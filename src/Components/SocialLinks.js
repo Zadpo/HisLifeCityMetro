037df0b9ff6647158ff8ba73968a50e1e6d81a0a
ZadrachCaunca <zadrachcaunca777@gmail.com>
2023-04-25 21:49:43 +0800

    initial commit

---
+++ src/Components/SocialLinks.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './SocialLinks.css';
+import Facebook from './images/Facebook.png';
+import Instagram from './images/Instagram.png';
+
+
+
+
+
+
+
+
+const SocialLinks = () => {
+  return (
+    <div className="social-links">
+      <a href="https://www.facebook.com/hislifemetro">
+        <img src={Facebook} alt="Facebook Icon" />
+      </a>
+      <div className="ig">
+        <a href="https://www.instagram.com/hislifemetro">
+        <img src={Instagram} alt="Instagram Icon" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default SocialLinks;
